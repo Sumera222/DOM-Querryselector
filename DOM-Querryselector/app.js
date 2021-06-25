2f64bc46e9ddfe0508c9515e796886d8d494038d
SUMERA222 <83210529+Sumera222@users.noreply.github.com>
2021-06-26 00:23:02 +0500

    Add files via upload

---
+++ DOM-Querryselector/app.js
@@ -0,0 +1,6 @@
+let d = document.querySelector("#First-div").className = "changed-class";
+let r = document.querySelector("#First-div").className;
+console.log(r);
+
+document.querySelector("#First-div").style.color = "red";
+document.querySelector("#First-div").style.border = "6px solid blue";
